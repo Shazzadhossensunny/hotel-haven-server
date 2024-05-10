118351327e3109efbb61b5b450714ef711f8ab7f
Shazzadhossensunny <shazzadhossensunny@gmail.com>
2024-05-10 16:42:05 +0600

    server set and get api create 2

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const app = express();
+var cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+var cookieParser = require('cookie-parser');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+
+// middleware
+const corsOptions = {
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:5174',
+      'https://hotel-haven-c883c.web.app/',
+    ],
+    credentials: true,
+    optionSuccessStatus: 200,
+  }
+app.use(cors(corsOptions))
+app.use(express.json());
+app.use(cookieParser())
+
+const uri = 'mongodb://localhost:27017';
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}L@cluster0.5kgqkgx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    const roomsCollection = client.db('hotelHavenDB').collection('rooms')
+
+    app.get("/rooms", async(req, res)=>{
+        const cursor = roomsCollection.find()
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+    app.get("/featuredRooms", async(req, res)=>{
+        const result = await roomsCollection.find().limit(6).toArray()
+        res.send(result)
+    })
+
+
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Hotel Haven is Running')
+  })
+
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+  })
